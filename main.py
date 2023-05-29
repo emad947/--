fd7ca527b1ce42bb75651bb8649ffdb7ea7a5c81
emad947 <134236202+emad947@users.noreply.github.com>
2023-05-29 08:21:52 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+a=500
+b=1000
+if a > b:
+    basic.show_string("a is greater than b")
+else:
+    basic.show_string("b greater than or equal t a")
---
+++ main.ts
@@ -0,0 +1,8 @@
+let a = 500
+let b = 1000
+if (a > b) {
+    basic.showString("a is greater than b")
+} else {
+    basic.showString("b greater than or equal t a")
+}
+
